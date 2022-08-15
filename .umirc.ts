e5e800ed3fbd5d9794a0260ceb5003fa06e2e81c
haoxiaoli <haoxiaoli@baidu.com>
2022-08-15 20:04:16 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -2,14 +2,6 @@ export default {
   npmClient: 'yarn',
   routes: [
     {path: '/', component: 'index'},
-    {path: '/spline', component: 'splineWidget'},
-    {path: '/reslice', component: 'resliceCursorWidget'},
-    {path: '/paint', component: 'paintWidget'},
-    {path: '/line', component: 'lineWidget'},
-    // {path: '/xml', component: 'XMLPolyDataWriter'},
-    {path: '/xml', component: 'XMLImageDataWriter'},
-    {path: '/shape', component: 'shapeWidget'},
-    {path: '/all', component: 'comprehensive'}
   ],
   proxy: {
     '/img': {
